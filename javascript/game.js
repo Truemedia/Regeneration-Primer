96309bd2d1e688b626cbdc43479fce150631eb85
Wade Penistone <truemedia@mediacityonline.net>
2013-01-13 22:07:08 +0000

    added jQuery + generic stylesheet + char selection screen cleanup

--- javascript/game.js
+++ javascript/game.js
@@ -1,3 +1,15 @@
+jQuery(document).ready(function (){
+	// When character chosen
+	jQuery(".char_select").click(function (){
+		/* Delete selection screen */
+		jQuery(".character_selection_row").remove();
+		/* Show option to bring up selection screen.. */
+		jQuery("#char_selection_screen").html("<a href='index.html'>Rechoose character</a>");
+		/* ..and Start the game up */
+		startGame(this.value);
+	});	
+});
+
 function startGame(character){
 	// Initializer
 	initGame();
