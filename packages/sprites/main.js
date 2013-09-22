34ac911a35322b9637a06ce00b604f995847268c
Wade Penistone <truemedia@mediacityonline.net>
2013-09-22 09:19:28 +0100

    fixed bug introduced from last commit (characters not loading)

--- packages/sprites/main.js
+++ packages/sprites/main.js
@@ -40,8 +40,8 @@ define(["./jQuery", "./Crafty", "./Config"], function(jQuery, Crafty, Config) {
 			/* Sprite maps for Characters */
 			// Directories
 			var characters_image_directory = sprites_dir + "characters/";
-			var characters_sprites_dir = characters_image_directory + Config.get('resources.sprites.default_sprite_filename_prefix');
-			var af = Config.get('resources.sprites.default_sprite_filename_suffix'); // Alternative filename.fileformat
+			var characters_sprites_dir = characters_image_directory + Config.get('resources.sprites.sprite_filename_prefix');
+			var af = Config.get('resources.sprites.sprite_filename_suffix'); // Alternative filename.fileformat
 			
 			Crafty.sprite(characters_sprites_dir+"Coward"+af, { coward: [0, 0, 128, 256] }); /* Coward */
 			Crafty.sprite(characters_sprites_dir+"Criminal"+af, { criminal: [0, 0, 128, 256] }); /* Criminal */
