d04f84ded6fed8c1b2da874df8979adb9646f334
Wade Penistone <truemedia@mediacityonline.net>
2013-12-26 19:19:55 +0000

    improved maps selection interface

--- packages/maps/nls/en/strings.js
+++ packages/maps/nls/en/strings.js
@@ -1,3 +1,6 @@
 define({
-    "": ""
+    "choose_map_phrs": "Choose a map",
+    "play_map_phrs": "Play Map",
+    "created_by_verb": "CREATED BY",
+    "likes_verb": "LIKES"
 });
--- packages/maps/nls/jp/strings.js
+++ packages/maps/nls/jp/strings.js
@@ -1,3 +1,6 @@
 define({
-    "": ""
+    "choose_map_phrs": "マップを選択してください",
+    "play_map_phrs": "マップをプレイ",
+    "created_by_verb": "によって作成された",
+    "likes_verb": "好き"
 });
