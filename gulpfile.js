d630aaacd943a43b1b2601dea232db846051f37d
Wade Penistone <truemedia@mediacityonline.net>
2014-05-20 21:01:39 +0100

    sprite and less watch watchers/builders

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,37 @@
-var gulp = require('gulp');
-var spritesmith = require('gulp.spritesmith');
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+	spritesmith = require('gulp.spritesmith'),
+	less = require('gulp-less'),
+	path = require('path');
 
 /* Compile map images into sprite and less file */
 gulp.task('sprite', function() {
-  console.log("Fusing visual assets into single entity");
-  var spriteData = gulp.src('maps/scraproom/tiles/*.png').pipe(spritesmith({
-    imgName: 'tileset.png',
-    cssName: 'stylesheet.less'
-  }));
-  spriteData.img.pipe(gulp.dest('maps/scraproom/'));
-  spriteData.css.pipe(gulp.dest('maps/scraproom/'));
+	console.log("Fusing visual assets into single entity");
+	var spriteData = gulp.src('maps/scraproom/tiles/*.png').pipe( spritesmith({
+		imgName: 'tileset.png',
+		cssName: 'stylesheet.less'
+	}));
+	spriteData.img.pipe(gulp.dest('maps/scraproom/'));
+	spriteData.css.pipe(gulp.dest('maps/scraproom/'));
 });
 
-gulp.task('default', ['sprite'], function() {
-  // place code for your default task here
-  console.log("Regeneration process initialized");
-  console.log("Rendering optimized internal programming");
+/* Generate CSS files from grouped LESS files */
+gulp.task('css', function() {
+	console.log("Rendering optimized internal programming");
+	gulp
+		.src('./stylesheets/default-theme/source.less')
+		.pipe( less({
+			paths: ['./maps/scraproom']
+		}))
+		.pipe(gulp.dest('./stylesheets/default-theme/'));
+});
+
+gulp.task('default', function() {
+	console.log("Regeneration process initialized");
+
+	// Watch files to trigger tasks
+	gulp.watch([
+		'./maps/scraproom/tiles/*.png',
+		'./stylesheets/default-theme/source.less'
+		], ['sprite', 'css']);
 });
