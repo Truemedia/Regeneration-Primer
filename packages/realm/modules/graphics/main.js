0370da708f853f7e72909e808df71a8a3ed80722
Wade Penistone <truemedia@mediacityonline.net>
2014-09-27 20:38:03 +0100

    Added XYZ debugging

--- packages/realm/modules/graphics/main.js
+++ packages/realm/modules/graphics/main.js
@@ -7,7 +7,7 @@
 * Git repo: {@link http://www.github.com/Truemedia/Regeneration-Primer| Regeneration Primer github repository}
 * Author links: {@link http://youtube.com/MCOMediaCityOnline| YouTube} and {@link http://github.com/Truemedia| Github}
 */
-define(['three'], function(THREE)
+define(['three', 'jQuery'], function(THREE, $)
 {
 	graphics =
 	{
@@ -24,17 +24,31 @@ define(['three'], function(THREE)
 		display: function(scene, cameras)
 		{
 			// Create animation loop
-			function animate(){
+			function animate()
+			{
 			  graphics.renderer.render(scene, cameras);
 
 			  var timer = Date.now() * 0.0005;
 			  cameras.position.x = Math.cos(timer) * 10;
 			  cameras.position.y = Math.sin(timer) * 10;
 			  cameras.lookAt(scene.position);
+			  graphics.debug(cameras.position);
 
 			  requestAnimationFrame(animate);
 			};
 			animate();
+		},
+
+		/* Show details of environment in debugging window */
+		debug: function(position)
+		{
+			var x = parseInt(position.x),
+				y = parseInt(position.y),
+				z = parseInt(position.z);
+
+			$('#x_pos').html(x);
+			$('#y_pos').html(y);
+			$('#z_pos').html(z);
 		}
 	};
 
